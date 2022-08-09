fb42879c0d8dcb6eb44c1524e4e9ed8a4a497ce2
juliwithoutthee <jnboucree@gmail.com>
2022-08-09 12:21:40 +0900

    Adds Navbar to Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+// import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +12,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Desserts App for the Stressed" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar></Navbar>
       <h1>Just Desserts</h1>
     </div>
   )
