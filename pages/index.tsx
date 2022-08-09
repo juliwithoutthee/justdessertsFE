6aacf44e2b0c7694f5c537b13d9bd343b754f3f7
juliwithoutthee <jnboucree@gmail.com>
2022-08-09 12:24:25 +0900

    Imports useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import React, { useState } from 'react';
 // import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/Navbar';
