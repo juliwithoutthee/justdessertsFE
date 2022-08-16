096861d36782ebb39867b19d42600e54f6ba2309
juliwithoutthee <jnboucree@gmail.com>
2022-08-16 09:57:59 +0900

    Creates elements to show test data

--- components/AllProducts.tsx
+++ components/AllProducts.tsx
@@ -2,23 +2,23 @@ import React, { useState } from "react";
 
 const testData = [
     {
-        productName: "",
-        description: "",
-        price: "",
+        productName: "Cake",
+        description: "A delicious three-tiered cake with buttercream frosting",
+        price: 3000,
         quantity: 0,
         image: ""
     },
     {
-        productName: "",
-        description: "",
-        price: "",
+        productName: "Brownies",
+        description: "Chocolatey brownies",
+        price: 2000,
         quantity: 0,
         image: ""
     },
     {
-        productName: "",
-        description: "",
-        price: "",
+        productName: "Cookies",
+        description: "Assorted cookies",
+        price: 1500,
         quantity: 0,
         image: ""
     }
@@ -29,5 +29,12 @@ const testData = [
 export default function AllProducts() {
 
     return <>
+        <div>
+            {testData.map((el: any) => {
+                return <div> <h2>{el.productName}</h2>
+                    <h3>{el.description}</h3>
+                    <h3>{el.price}</h3> </div>;
+            })}
+        </div>
     </>
 }
