e6b6a44452d931a6bb466e83077078cbf13ec8e6
juliwithoutthee <jnboucree@gmail.com>
2022-08-16 10:36:49 +0900

    Adds images to testData

--- components/AllProducts.tsx
+++ components/AllProducts.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+//import Brownies from "./images/Brownies.jpeg";
 
 const testData = [
     {
@@ -6,21 +7,21 @@ const testData = [
         description: "A delicious three-tiered cake with buttercream frosting",
         price: 3000,
         quantity: 0,
-        image: ""
+        image: "./images/CarrotCake.webp"
     },
     {
         productName: "Brownies",
         description: "Chocolatey brownies",
         price: 2000,
         quantity: 0,
-        image: ""
+        image: "./images/Brownies.jpeg"
     },
     {
         productName: "Cookies",
         description: "Assorted cookies",
         price: 1500,
         quantity: 0,
-        image: ""
+        image: "./images/ChocolateChipCookies.jpeg"
     }
 ];
 
@@ -30,10 +31,13 @@ export default function AllProducts() {
 
     return <>
         <div>
+            <img src="./images/Brownies.jpeg" />
             {testData.map((el: any) => {
                 return <div className="item"> <h2>{el.productName}</h2>
                     <h3>{el.description}</h3>
-                    <h3>{el.price}</h3> </div>;
+                    <h3>{el.price}</h3>
+                    <img src={el.image} ></img>
+                </div>;
             })}
         </div>
     </>
